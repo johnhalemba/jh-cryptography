03ea1a8b921dbf289a6d559ea52871cc39c6f1c3
Jan Halemba <jan.jakub.halemba@proton.me>
2025-10-21 12:24:05 +0200

    forgot to add wallet implementation...

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,19 +1,28 @@
+from wallet.key_manager import KeyManager
+
 class Wallet:
+    """Cyfrowy portfel użytkownika"""
 
     def __init__(self, wallet_name: str):
         self.name = wallet_name
+        self.key_manager = KeyManager(wallet_name)
 
     def create_wallet(self, password: str):
-        pass
+        """Tworzy nowy portfel"""
+        self.key_manager.create_new_identity(password)
 
     def open_wallet(self, password: str):
-        pass
+        """Otwiera istniejący portfel"""
+        self.key_manager.load_identity(password)
 
     def get_address(self) -> str:
-        pass
+        """Zwraca adres portfela"""
+        return self.key_manager.get_address()
 
     def sign_transaction(self, tx_data: bytes) -> str:
-        pass
+        """Podpisuje transakcję"""
+        return self.key_manager.sign_data(tx_data)
 
     def get_public_key(self) -> str:
-        pass
+        """Zwraca klucz publiczny"""
+        return self.key_manager.get_public_key_hex()
